78d4d9f8d43de4626f18249d831e7ccd55d86cb8
H1brian <133236039+H1brian@users.noreply.github.com>
2024-05-26 22:42:55 -0400

    Add header footer note component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Note from "./components/Note.jsx";
+
+const App = () => {
+    return <main>
+    <Header />
+    <Note />
+    <Footer />
+    </main>
+};
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+    return <footer>
+        <p>Brian Geng @Copyright {new Date().getFullYear()}</p>
+    </footer>
+}
+
+export default Footer;
