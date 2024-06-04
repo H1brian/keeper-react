6f36526a2ce9440202dd69c90131eaff81a05167
H1brian <133236039+H1brian@users.noreply.github.com>
2024-06-03 22:44:29 -0400

    Add dynamic notes mapping function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Note from "./components/Note.jsx";
+import notes from "./notes.js";
 
 const App = () => {
     return <main>
     <Header />
-    <Note />
+    {notes.map( noteEntry => <Note 
+        key={noteEntry.key}
+        title={noteEntry.title}
+        description={noteEntry.content}
+    />)}
     <Footer />
     </main>
 };
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,5 @@ import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+root.render(<App /> );
 
